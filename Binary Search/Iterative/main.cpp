e283899110e17157116bdb5852f586cf4fe2b717
tushaaaaar <tushars202003@gmail.com>
2024-06-15 13:11:49 +0530

    Iterative Binary Search Added

---
+++ Binary Search/Iterative/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool binary_search(vector<int>& arr, int l, int r, int x)
+{
+    while (l <= r)
+    {
+        int mid = l + (r-l)/2;
+        if (x == arr[mid]) return true;
+        else if (x < arr[mid]) r = mid - 1;
+        else l = mid + 1;
+    }
+    return false;
+}
+
+int main ()
+{
+    int n, x;
+    vector<int> arr;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int num;
+        cin >> num;
+        arr.push_back(num);
+    }
+    cout << "Enter element you want to search: ";
+    cin >> x;
+    if (binary_search(arr, 0, n-1, x))
+        cout << "Element Present in Array" << endl;
+    else
+        cout << "Element is not Present in Array" << endl;
+     return 0;
+}
