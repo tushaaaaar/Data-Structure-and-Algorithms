2a6f8c5272a473e037b51181a2a184b61baacfcd
tushaaaaar <tushars202003@gmail.com>
2024-07-29 20:37:21 +0530

    Added Polynomial List

---
+++ Polynomial Linked List/Polynomial List/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int coeff;
+    int exp;
+    Node* next;
+
+    Node (int coeff, int exp) : coeff(coeff), exp(exp), next(NULL) {}
+};
+
+class ListNode
+{
+private:
+    Node* head;
+
+public:
+    ListNode () : head(NULL) {}
+
+    void polynomial(int coeff, int exp)
+    {
+        if (coeff == 0)
+            return;
+        
+        Node* poly = new Node(coeff, exp);
+        if (head == NULL)
+            head = poly;
+        else
+        {
+            poly->next = head;
+            head = poly;
+        }
+    }
+
+    void display()
+    {
+        Node* current = head;
+        while (current != NULL)
+        {
+            if (current->next == NULL && current->exp != 0)
+                cout << current->coeff << "*x^" << current->exp;
+            else if (current->exp != 0)
+                cout << current->coeff << "*x^" << current->exp << " + ";
+            else
+                cout << current->coeff;
+            current = current->next;
+        }
+    }
+};
+
+int main()
+{
+    ListNode list;
+    int n;
+    cout << "Enter degree of polynomial: ";
+    cin >> n;
+    for (int i = 0; i <= n; i++)
+    {
+        int coeff;
+        cout << "Enter coefficient of x^" << i << ": ";
+        cin >> coeff;
+        list.polynomial(coeff, i);
+    }
+    list.display();
+}
